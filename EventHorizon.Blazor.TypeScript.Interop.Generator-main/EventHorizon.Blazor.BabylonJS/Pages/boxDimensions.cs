8be6cab9417df5b7b8691312c28a83784813ee4d
danieljpearce <danjpearce678@gmail.com>
2022-04-20 12:46:45 +0100

    Improved form validation

--- EventHorizon.Blazor.TypeScript.Interop.Generator-main/EventHorizon.Blazor.BabylonJS/Pages/boxDimensions.cs
+++ EventHorizon.Blazor.TypeScript.Interop.Generator-main/EventHorizon.Blazor.BabylonJS/Pages/boxDimensions.cs
@@ -6,15 +6,15 @@ namespace EventHorizon.Blazor.BabylonJS.Pages;
 class boxDimensions
 {
     [Required(ErrorMessage = "Box Width Required")]
-    [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+    [Range(0.1,1.2, ErrorMessage ="Value must be between 0.1 and the pallet width")]
     public decimal boxX { get; set; }
 
     [Required(ErrorMessage = "Box Height Required")]
-    [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+    [Range(0.1, 1, ErrorMessage = "Value must be between 0.1 and the pallet height")]
     public decimal boxY { get; set; }
 
     [Required(ErrorMessage = "Box Width Required")]
-    [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+    [Range(0.1, 1, ErrorMessage = "Value must be between 0.1 and the pallet length")]
     public decimal boxZ { get; set; }
 
     public bool useStaircase { get; set; } 
